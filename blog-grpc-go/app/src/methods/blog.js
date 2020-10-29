4be31a1d93d527453badee6492b1d31d31e2fa35
vikashsurin <vikashsurin10@gmail.com>
2020-10-29 20:51:43 +0530

    add delete and list blogs

--- blog-grpc-go/app/src/methods/blog.js
+++ blog-grpc-go/app/src/methods/blog.js
@@ -4,46 +4,57 @@ const {
   ReadBlogRequest,
   UpdateBlogRequest,
   DeleteBlogRequest,
+  ListBlogRequest,
 } = require("../protos/blog_pb");
-const { BlogServiceClient } = require("../protos/blog_grpc_web_pb");
+const {
+  BlogServiceClient,
+  default: blog,
+} = require("../protos/blog_grpc_web_pb");
 
 export var blogID = "5f8043e7f5edbb1673a50f48";
+const host = "http://localhost:8000";
 
 export function CreateBlog() {
-  var client = new BlogServiceClient("http://localhost:8080");
+  var client = new BlogServiceClient(host);
 
   var blog = new Blog();
   blog.setAuthorId("vikash surin");
-  blog.setTitle("new blog");
+  blog.setTitle("new blog from react");
   blog.setContent("This is a new content.");
 
   var request = new CreateBlogRequest();
   request.setBlog(blog);
-  
-    console.log("create blog")
 
-    var call = client.createBlog(request, {}, (err, response) => {
-      console.log("CREATE BLOG :: ", response);
-      if (err){
-        console.log(err.code)
-        console.log(err.message)
-      }else{
-        console.log(response.getBlog())
-      }
-    });
-    call.on("status",function(status){
+  console.log("create blog");
+  client.createBlog(request, {}, (err, response) => {
+    var blogObj = {
+      authorId: response.getBlog().getAuthorId(),
+      title: response.getBlog().getTitle(),
+      content: response.getBlog().getContent(),
+    };
+    console.log("response ", blogObj);
+  });
 
-      console.log(status.code);
-      console.log(status.details);
-      console.log(status.metadata);
-    })
- 
+  // var call = client.createBlog(request, {}, (err, response) => {
+  //   console.log("CREATE BLOG :: ", response);
+  //   if (err){
+  //     console.log(err.code)
+  //     console.log(err.message)
+  //   }else{
+  //     console.log(response.getBlog())
+  //   }
+  // });
+  // call.on("status",function(status){
+  //   console.log(status.code);
+  //   console.log(status.details);
+  //   console.log(status.metadata);
+  // })
 
   console.log("create blog exited");
 }
 
 export function ReadBlog() {
-  var client = new BlogServiceClient("http://localhost:8080");
+  var client = new BlogServiceClient(host);
   var request = new ReadBlogRequest();
   request.setBlogId(blogID);
   client.readBlog(request, {}, (err, response) => {
@@ -52,7 +63,7 @@ export function ReadBlog() {
 }
 
 export function UpdateBlog() {
-  var client = new BlogServiceClient("http://localhost:8080");
+  var client = new BlogServiceClient(host);
 
   var blog = new Blog();
   blog.setId(blogID);
@@ -69,12 +80,46 @@ export function UpdateBlog() {
 }
 
 export function DeleteBlog() {
-  var client = new BlogServiceClient("http://localhost:8080");
+  var client = new BlogServiceClient(host);
 
   var request = new DeleteBlogRequest();
   request.setBlogId(blogID);
 
   client.deleteBlog(request, {}, (err, response) => {
     console.log("DELETED BLOG :: ", response);
+  });
+}
+export function listblog() {
+  console.log("list blog called");
+  var client = new BlogServiceClient(host);
+
+  var request = new ListBlogRequest();
+  var stream = client.listBlog(request, {});
+
+  // create empty array of blogs object
+  var blogs = new Array();
+
+  stream.on("data", (response) => {
+    var blogObj = {
+      id: response.getBlog().getId(),
+      authorId: response.getBlog().getAuthorId(),
+      title: response.getBlog().getTitle(),
+      content: response.getBlog().getContent(),
+    };
+
+    // push each blog object to blogs array.
+    blogs.push(blogObj);
+  });
+
+  console.log("array ", blogs);
+
+  stream.on("status", function (status) {
+    console.log(status.code);
+    console.log(status.details);
+    console.log(status.metadata);
+  });
+  stream.on("end", function (end) {
+    // stream end signal
+    console.log("steam end");
   });
 }
