b36b469ee13f2e37c98ac7f63ec028c2e1c87d65
vikashsurin <vikashsurin10@gmail.com>
2020-10-30 12:56:54 +0530

    adding redux

--- blog-grpc-go/app/src/index.js
+++ blog-grpc-go/app/src/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "./redux/store";
-
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
