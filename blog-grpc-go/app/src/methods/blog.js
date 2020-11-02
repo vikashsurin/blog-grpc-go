86d6f0aeb3d3633d723ea956bcfc58938c3b8164
vikashsurin <vikashsurin10@gmail.com>
2020-11-02 22:58:18 +0530

    update blog mehthods

--- blog-grpc-go/app/src/methods/blog.js
+++ blog-grpc-go/app/src/methods/blog.js
@@ -1,3 +1,5 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
 const {
   Blog,
   CreateBlogRequest,
@@ -7,6 +9,7 @@ const {
   ListBlogRequest,
 } = require("../protos/blog_pb");
 const {
+  BlogServicePromiseClient,
   BlogServiceClient,
   default: blog,
 } = require("../protos/blog_grpc_web_pb");
@@ -14,73 +17,60 @@ const {
 export var blogID = "5f8043e7f5edbb1673a50f48";
 const host = "http://localhost:8000";
 
-export function CreateBlog() {
-  var client = new BlogServiceClient(host);
+export const CreateBlog = async (author, title, content) => {
+  var client = new BlogServicePromiseClient(host, null, null);
 
   var blog = new Blog();
-  blog.setAuthorId("vikash surin");
-  blog.setTitle("new blog from react");
-  blog.setContent("This is a new content.");
+  blog.setAuthorId(author);
+  blog.setTitle(title);
+  blog.setContent(content);
 
   var request = new CreateBlogRequest();
   request.setBlog(blog);
 
-  console.log("create blog");
-  client.createBlog(request, {}, (err, response) => {
-    var blogObj = {
-      authorId: response.getBlog().getAuthorId(),
-      title: response.getBlog().getTitle(),
-      content: response.getBlog().getContent(),
-    };
-    console.log("response ", blogObj);
-  });
+  try {
+    const res = await client.createBlog(request, {});
+    console.log(res.getBlog().getId());
+    return res.getBlog();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 
-  // var call = client.createBlog(request, {}, (err, response) => {
-  //   console.log("CREATE BLOG :: ", response);
-  //   if (err){
-  //     console.log(err.code)
-  //     console.log(err.message)
-  //   }else{
-  //     console.log(response.getBlog())
-  //   }
-  // });
-  // call.on("status",function(status){
-  //   console.log(status.code);
-  //   console.log(status.details);
-  //   console.log(status.metadata);
-  // })
-
-  console.log("create blog exited");
-}
-
-export function ReadBlog() {
-  var client = new BlogServiceClient(host);
+export const ReadBlog = async (id) => {
+  var client = new BlogServicePromiseClient(host);
   var request = new ReadBlogRequest();
-  request.setBlogId(blogID);
-  client.readBlog(request, {}, (err, response) => {
-    console.log("READ BLOG ::  ", response);
-  });
-}
-
-export function UpdateBlog() {
-  var client = new BlogServiceClient(host);
+  request.setBlogId(id);
+  try {
+    const res = await client.readBlog(request, {}, (err, response) => {
+      console.log("READ BLOG ::  ", response);
+    });
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+export const UpdateBlog = async (id, title, content) => {
+  var client = new BlogServicePromiseClient(host);
 
   var blog = new Blog();
-  blog.setId(blogID);
+  blog.setId(id);
   blog.setAuthorId("vikash");
-  blog.setTitle("updated blog from react");
-  blog.setContent("This is a new Updated content.");
+  blog.setTitle(title);
+  blog.setContent(content);
 
   var request = new UpdateBlogRequest();
   request.setBlog(blog);
 
-  client.updateBlog(request, {}, (err, response) => {
+  const res = await client.updateBlog(request, {}, (err, response) => {
     console.log("UPDATE BLOG :: ", response);
   });
-}
+  return res;
+};
 
-export function DeleteBlog() {
-  var client = new BlogServiceClient(host);
+export const DeleteBlog = async () => {
+  var client = new BlogServicePromiseClient(host);
 
   var request = new DeleteBlogRequest();
   request.setBlogId(blogID);
@@ -88,38 +78,13 @@ export function DeleteBlog() {
   client.deleteBlog(request, {}, (err, response) => {
     console.log("DELETED BLOG :: ", response);
   });
-}
-export function listblog() {
-  console.log("list blog called");
+};
+
+export const listblog = () => {
   var client = new BlogServiceClient(host);
 
   var request = new ListBlogRequest();
   var stream = client.listBlog(request, {});
 
-  // create empty array of blogs object
-  var blogs = new Array();
-
-  stream.on("data", (response) => {
-    var blogObj = {
-      id: response.getBlog().getId(),
-      authorId: response.getBlog().getAuthorId(),
-      title: response.getBlog().getTitle(),
-      content: response.getBlog().getContent(),
-    };
-
-    // push each blog object to blogs array.
-    blogs.push(blogObj);
-  });
-
-  console.log("array ", blogs);
-
-  stream.on("status", function (status) {
-    console.log(status.code);
-    console.log(status.details);
-    console.log(status.metadata);
-  });
-  stream.on("end", function (end) {
-    // stream end signal
-    console.log("steam end");
-  });
-}
+  return stream;
+};
