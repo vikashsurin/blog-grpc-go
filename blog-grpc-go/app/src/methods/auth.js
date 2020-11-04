ed72162a578c927470e08fec6161dbaa73629979
vikashsurin <vikashsurin10@gmail.com>
2020-11-04 12:36:52 +0530

    add auth

--- blog-grpc-go/app/src/methods/auth.js
+++ blog-grpc-go/app/src/methods/auth.js
@@ -1,22 +1,22 @@
 const { LoginRequest, LogoutRequest } = require("../protos/auth_pb");
-const { AuthServiceClient } = require("../protos/auth_grpc_web_pb");
+const { AuthServicePromiseClient } = require("../protos/auth_grpc_web_pb");
 
 const host = "http://localhost:8000";
 
-export function login() {
-  var client = new AuthServiceClient(host);
+export const Login = async (email, password) => {
+  var client = new AuthServicePromiseClient(host);
 
   var request = new LoginRequest();
-  request.setUserEmail("vikashsurin10@gmail.com");
-  request.setPassword("password");
+  request.setUserEmail(email);
+  request.setPassword(password);
 
   client.login(request, {}, (err, response) => {
     console.log("login res", response);
   });
-}
+};
 
 export function logout() {
-  var client = new AuthServiceClient(host);
+  var client = new AuthServicePromiseClient(host);
 
   var request = new LogoutRequest();
 
